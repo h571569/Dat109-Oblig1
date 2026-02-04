9635645147ee6bf5730b8932a9e7abdde45fc504
h571569 <571569@stud.hvl.no>
2026-02-04 15:58:59 +0100

    Test

--- src/main/java/no/hvl/dat109/Brett.java
+++ src/main/java/no/hvl/dat109/Brett.java
@@ -1,4 +1,8 @@
 package no.hvl.dat109;
 
 public class Brett {
+
+    public int trill() {
+        return 1;
+    }
 }
---
+++ src/test/java/no/hvl/dat109/BrettTest.java
@@ -0,0 +1,10 @@
+package no.hvl.dat109;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BrettTest {
+
+    @org.junit.jupiter.api.Test
+    void trill() {
+    }
+}
