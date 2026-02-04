221850d0bd3459ed4113f8f60cd2a2dd583e488e
h184920 <itunesglambek@gmail.com>
2026-02-04 18:19:04 +0100

    Update Brett.java

--- src/main/java/no/hvl/dat109/Brett.java
+++ src/main/java/no/hvl/dat109/Brett.java
@@ -1,13 +1,7 @@
 package no.hvl.dat109;
 
 public class Brett {
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
     return(int)()
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 
     public int trill() {
         return 0;
