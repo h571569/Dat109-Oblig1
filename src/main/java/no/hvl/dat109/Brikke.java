d6ddb9f21a82c257856d56085fab80a1d31fa79f
h184906 <184906@stud.hvl.no>
2026-02-04 17:15:39 +0100

    okokook

--- src/main/java/no/hvl/dat109/Brikke.java
+++ src/main/java/no/hvl/dat109/Brikke.java
@@ -2,6 +2,6 @@ package no.hvl.dat109;
 
 public class Brikke {
     public static void main(String[] args) {
-        System.out.println("Verden lever");
+        System.out.println("Verden lever ikke lengre");
     }
 }
