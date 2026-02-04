5f14d04bbd093b1d1e66385a7bf1023604926790
h184906 <184906@stud.hvl.no>
2026-02-04 21:27:54 +0100

    fix fjes

--- src/main/java/no/hvl/dat109/Terning.java
+++ src/main/java/no/hvl/dat109/Terning.java
@@ -4,7 +4,7 @@ import no.hvl.dat109.grensefjes.TerningFjes;
 
 public class Terning implements TerningFjes {
 
-    public int trill() {
+    public static int trill() {
         return(int)(Math.random() * 6) + 1;
     }
 }
