5a8fa95405548fc21519cd0209f0142da6b70662
h184906 <184906@stud.hvl.no>
2026-02-04 20:40:23 +0100

    halveis

--- src/main/java/no/hvl/dat109/Spill.java
+++ src/main/java/no/hvl/dat109/Spill.java
@@ -3,5 +3,24 @@ package no.hvl.dat109;
 public class Spill {
     private Terning terning;
     private Brett brett;
+    private int antallSpillerer;
+    private boolean vunnet;
 
+
+    public Spill(Terning terning, Brett brett, int antallSpillerer) {
+        this.terning = terning;
+        this.brett = brett;
+        this.antallSpillerer = antallSpillerer;
+    }
+
+    public void spill() {
+        
+        while (!vunnet) {
+            
+            for (int i = 1; i < antallSpillerer+1; i++) {
+                
+            }
+
+        }
+    }
 }
