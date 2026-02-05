965c2893d98d9b44d8193b6eedf7dd662184f230
h184906 <184906@stud.hvl.no>
2026-02-05 13:39:05 +0100

    Enter

--- src/main/java/no/hvl/dat109/Spill.java
+++ src/main/java/no/hvl/dat109/Spill.java
@@ -1,11 +1,11 @@
 package no.hvl.dat109;
 
+import java.util.Scanner;
+
 public class Spill {
     // private Brett brett;
     private final int antallSpillerer;
     private boolean vunnet = false;
-
-
 
     public Spill(final int antallSpillerer) {
         this.antallSpillerer = antallSpillerer;
@@ -19,25 +19,35 @@ public class Spill {
 
         final Brikke[] brikker = new Brikke[antallSpillerer];
         for (int x = 0; x < antallSpillerer; x++) {
-            brikker[x] = new Brikke(0, x+1);
+            brikker[x] = new Brikke(0, x + 1);
         }
+
+        Scanner scanner = new Scanner(System.in); 
 
         while (!vunnet) {
-            
-            for (int i = 0; i < antallSpillerer && (!vunnet); i++) {
-                int sum = Terning.trill();
-                if (brikker[i].getPosisjon()+sum < 100) {
-                    brikker[i].setPosisjon(brikker[i].getPosisjon()+sum);
-                    System.out.println(brikker[i].getBrikkeId()+ " er på rute "+ brikker[i].getPosisjon()+ " og fikk terningkast: " + sum);
-                } else if (brikker[i].getPosisjon()+sum == 100) {
-                    System.out.println("Brikke: "+ brikker[i].getBrikkeId()+ " Har vunnet!");
-                    vunnet = true;
-                } else {
-                    System.out.println(brikker[i].getBrikkeId()+ " fikk for mye!");
-                }
-                sum = 0;
-            }
+            for (int i = 0; i < antallSpillerer && !vunnet; i++) {
 
+                System.out.println("Brikke nr: " + brikker[i].getBrikkeId()
+                        + " sin tur. Trykk Enter for å fortsette..");
+                scanner.nextLine(); 
+
+                int sum = Terning.trill();
+
+                if (brikker[i].getPosisjon() + sum < 100) {
+                    brikker[i].setPosisjon(brikker[i].getPosisjon() + sum);
+                    System.out.println(brikker[i].getBrikkeId() + " er på rute "
+                            + brikker[i].getPosisjon() + " og fikk terningkast: " + sum);
+
+                } else if (brikker[i].getPosisjon() + sum == 100) {
+                    System.out.println("Brikke: " + brikker[i].getBrikkeId() + " Har vunnet!");
+                    vunnet = true;
+
+                } else {
+                    System.out.println(brikker[i].getBrikkeId() + " fikk for mye!");
+                }
+            }
         }
+
+        scanner.close(); 
     }
 }
